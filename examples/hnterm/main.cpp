cbb00623dad324dcd74f54dfe9fb1072294a54ba
Georgi Gerganov <ggerganov@gmail.com>
2019-12-14 21:26:17 +0200

    [hnterm] ..

--- examples/hnterm/main.cpp
+++ examples/hnterm/main.cpp
@@ -919,15 +919,13 @@ extern "C" {
                 ImGui::End();
             }
 
-            //if (ImGui::IsKeyPressed('h', true) ||
-            //    ImGui::IsKeyPressed(ImGui::GetIO().KeyMap[ImGuiKey_LeftArrow], true)) {
-            //    stateUI.hoveredWindowId = std::max(0, stateUI.hoveredWindowId - 1);
-            //}
+            if (ImGui::IsKeyPressed(ImGui::GetIO().KeyMap[ImGuiKey_LeftArrow], true)) {
+                stateUI.hoveredWindowId = std::max(0, stateUI.hoveredWindowId - 1);
+            }
 
-            //if (ImGui::IsKeyPressed('l', true) ||
-            //    ImGui::IsKeyPressed(ImGui::GetIO().KeyMap[ImGuiKey_RightArrow], true)) {
-            //    stateUI.hoveredWindowId = std::min(stateUI.nWindows - 1, stateUI.hoveredWindowId + 1);
-            //}
+            if (ImGui::IsKeyPressed(ImGui::GetIO().KeyMap[ImGuiKey_RightArrow], true)) {
+                stateUI.hoveredWindowId = std::min(stateUI.nWindows - 1, stateUI.hoveredWindowId + 1);
+            }
 
             if (stateUI.showStatusWindow) {
                 {
